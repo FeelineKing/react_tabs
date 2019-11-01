549ca7681a80c26a27ace56009e98eeddcbf9a19
Leon <FeelineKing@gmail.com>
2019-11-01 04:58:04 +0200

    Updates

---
+++ precache-manifest.40270694ac909455aded64ea10d75c52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cd2527dc0c2033a491dc1291d4c742",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "f056bfa56d86ccab94d8",
+    "url": "/react_tabs/static/css/main.dd6ba41a.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/react_tabs/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "f056bfa56d86ccab94d8",
+    "url": "/react_tabs/static/js/main.b7323545.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
